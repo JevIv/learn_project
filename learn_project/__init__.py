281db0caa3e54dd397b380eaeba267b9c4e73599
ivan <skeap@cn.ru>
2020-06-13 00:53:34 +0700

    правки по линтеру

--- learn_project/__init__.py
+++ learn_project/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 import config as cfg
 
-def create_app():
-    app = Flask(__name__) # создает экземпляр Flask в переменной app
-    app.config.from_pyfile('config.py') # задает файл конфигурационный файл
-    @app.route('/') # путь, перейдя по котрому запустится app
 
-    def index(): # возвращает стартовую страничку
-        return render_template ('index.html')
-    
-    return app # возвращает экземпляр приложения Flask
+def create_app():
+    app = Flask(__name__)  # создает экземпляр Flask в переменной app
+    app.config.from_pyfile('config.py')  # задает файл конфигурационный файл
+    @app.route('/')  # путь, перейдя по котрому запустится app
+    def index():  # возвращает стартовую страничку
+        return render_template('index.html')
+    return app  # возвращает экземпляр приложения Flask
--- learn_project/get_html.py
+++ learn_project/get_html.py
@@ -2,28 +2,27 @@ from bs4 import BeautifulSoup as bs
 import re
 import requests
 
-# Забирает одну страницу
-def get_html(url):
+
+def get_html(url):  # Забирает одну страницу
     try:
-        response = requests.get(url) # сюда падает текст из ответа на get
+        response = requests.get(url)  # сюда падает текст из ответа на get
         response.raise_for_status()
         return response.text
     except(requests.RequestException, ValueError):
         return False
 
-# Находит номер последней страницу в категории 
-def last_page(text):
+
+def last_page(text):  # Находит номер последней страницу в категории 
     soup = bs(text, 'html.parser')
-    pagination = soup.find_all('span', class_ = "pagination-item-1WyVp") # Находит 'кнопки' в разделе pagination
-    digits = re.findall(r'\d{2}', pagination[-2]['data-marker'],) # Вытаскивает цифры из предпоследней кнопки
-    last_page = int(digits[0]) # Преобрузует цифры в int
+    pagination = soup.find_all('span', class_="pagination-item-1WyVp")  # Находит 'кнопки' в разделе pagination
+    digits = re.findall(r'\d{2}', pagination[-2]['data-marker'],)  # Вытаскивает цифры из предпоследней кнопки
+    last_page = int(digits[0])  # Преобрузует цифры в int
     return last_page
 
-# Делает итерацию по страницам
-def get_all_pages(url):
-    all_pages = []
-    max_page = last_page(get_html(url)) # Определает, сколько циклу надо итераций
-    for page_number in range(1, max_page):
-        all_pages.append(get_html(url, page_number)) # Добавляет страницу (str) как элемент списка 
-    return all_pages
 
+def get_all_pages(url):  # Делает итерацию по страницам
+    all_pages = []
+    max_page = last_page(get_html(url))  # Определает, сколько циклу надо итераций
+    for page_number in range(1, max_page):
+        all_pages.append(get_html(url, page_number))  # Добавляет страницу (str) как элемент списка 
+    return all_pages
