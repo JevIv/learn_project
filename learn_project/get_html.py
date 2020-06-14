5a1522bdc927f75427f1a0f2f8e6110c49ac952f
ivan <skeap@cn.ru>
2020-06-14 18:23:48 +0700

    добавил get по номеру страницы

--- learn_project/get_html.py
+++ learn_project/get_html.py
@@ -3,16 +3,17 @@ import re
 import requests
 
 
-def get_html(url):  # Забирает одну страницу
+def get_html(url, page_number=1):  # Забирает одну страницу
+    params = {'p': page_number}
     try:
-        response = requests.get(url)  # сюда падает текст из ответа на get
+        response = requests.get(url, params=params)  # сюда падает текст из ответа на get
         response.raise_for_status()
         return response.text
     except(requests.RequestException, ValueError):
         return False
 
 
-def last_page(text):  # Находит номер последней страницу в категории 
+def last_page(text):  # Находит номер последней страницу в категории
     soup = bs(text, 'html.parser')
     pagination = soup.find_all('span', class_="pagination-item-1WyVp")  # Находит 'кнопки' в разделе pagination
     digits = re.findall(r'\d{2}', pagination[-2]['data-marker'],)  # Вытаскивает цифры из предпоследней кнопки
@@ -24,5 +25,5 @@ def get_all_pages(url):  # Делает итерацию по страницам
     all_pages = []
     max_page = last_page(get_html(url))  # Определает, сколько циклу надо итераций
     for page_number in range(1, max_page):
-        all_pages.append(get_html(url, page_number))  # Добавляет страницу (str) как элемент списка 
+        all_pages.append(get_html(url, page_number))  # Добавляет страницу (str) как элемент списка
     return all_pages
