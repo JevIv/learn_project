47e15a075205dc57929a55153330f070490f9f59
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-06-14 09:41:00 +0100

    Убрал exceptions, теперь, если не нашёл ифы по переменной, вернёт None, добавил обработку ошибки на строке 30 и дописал возврат словаря

--- product_details.py
+++ product_details.py
@@ -10,45 +10,31 @@ def get_html(url):
 def get_product_details(html):
  	if html:
  		soup = bs(html, 'html.parser')
- 		product = soup.find('div', class_ = 'item-view js-item-view')	#контейнер с товаром
- 		try:															#все поиски ниже ведутся в нём
- 			name = product.find('span', class_ = 'title-info-title-text').text
- 		except:
- 			name = ''
- 		try:
- 			price = product.find('span', class_ = 'js-item-price').text
- 		except:
- 			price = ''
- 		try:
- 			date = product.find('div', class_ = 'title-info-metadata-item-redesign').text
- 		except:
- 			date = ''
- 		try:
- 			text = product.find('div', class_ = 'item-description-text').text
- 		except:
- 			text = ''
- 		try:
- 			address = product.find('span', class_ = 'item-address__string').text
- 		except:
- 			address = ''
- 		try:
- 			ad_number = product.find('div', class_ = 'item-view-search-info-redesign').find('span').text
- 		except:
- 			ad_number = ''
+ 		product = soup.find('div', class_ = 'item-view js-item-view')	#контейнер с товаром, все поиски ниже ведутся в нём
+ 		name = product.find('span', class_ = 'title-info-title-text').text
+ 		price = product.find('span', class_ = 'js-item-price').text
+ 		date = product.find('div', class_ = 'title-info-metadata-item-redesign').text
+ 		text = product.find('div', class_ = 'item-description-text').text
+ 		address = product.find('span', class_ = 'item-address__string').text
+ 		ad_number = product.find('div', class_ = 'item-view-search-info-redesign').find('span').text
  		images_urls = []	#ниже идёт проверка на картинки,
  		try:				#сначала проверяет на список приложенных картинок, 
  							#если нету, то забирает главную картинку
 	 		gallery_list = product.find('ul', class_ = 'gallery-list js-gallery-list')
 	 		images = gallery_list.findAll('img')
+
 	 		for image in images:
 	 			urls = image['src']
 	 			images_urls.append(urls)
-	 	except:
+
+	 	except(AttributeError):
 	 		main_image = product.find('div', class_ = 'item-view-gallery item-view-gallery_type-one-img')
 	 		images = main_image.findAll('img')
+
 	 		for image in images:
 	 			urls = image['src']
 	 			images_urls.append(urls)
+
 
 	 	details = {			#словарь в который собираются данные
 	 		'name':name,
@@ -58,8 +44,9 @@ def get_product_details(html):
 	 		'address':address,
 	 		'ad_number':ad_number,
 	 		'images_urls':images_urls}
+	 	return details
 
 
 if __name__ == '__main__':
-	url = "https://www.avito.ru/moskva/tovary_dlya_kompyutera/videokarta_rx_470_4_gb_asus_strix_1945905515"
+	url = "https://www.avito.ru/moskva/tovary_dlya_kompyutera/gigabyte_rx560_2gb_dual_asus_gtx1050_2g_strix_1861086660"
 	get_product_details(get_html(url))
