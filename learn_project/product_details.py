73d02b3983ad181a6e02a3236ab741fb21153c03
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-06-17 17:54:24 +0100

    obrabot4ik o6ibok v tegah

--- learn_project/product_details.py
+++ learn_project/product_details.py
@@ -7,15 +7,18 @@ from learn_project.get_html import get_html
 def get_product_details(html):
 	if not html:
 		raise ValueError
-	soup = bs(html, 'html.parser')
-	product = soup.find('div', class_='item-view js-item-view')	 # контейнер с товаром, все поиски ниже ведутся в нём
-	name = product.find('span', class_='title-info-title-text').text
-	price = product.find('span', class_='js-item-price').text
-	date = product.find('div', class_='title-info-metadata-item-redesign').text
-	text = product.find('div', class_='item-description-text').text
-	address = product.find('span', class_='item-address__string').text
-	ad_number = product.find('div', class_='item-view-search-info-redesign').find('span').text
-	images_urls = []  # ниже идёт проверка на картинки,
+	try:
+		soup = bs(html, 'html.parser')
+		product = soup.find('div', class_='item-view js-item-view')	 # контейнер с товаром, все поиски ниже ведутся в нём
+		name = product.find('span', class_='title-info-title-text').text
+		price = product.find('span', class_='js-item-price').text
+		date = product.find('div', class_='title-info-metadata-item-redesign').text
+		text = product.find('div', class_='item-description-text').text
+		address = product.find('span', class_='item-address__string').text
+		ad_number = product.find('div', class_='item-view-search-info-redesign').find('span').text
+		images_urls = []  # ниже идёт проверка на картинки,
+	except(AttributeError):
+		print("Не нашёл теги для переменной")
 	try:			  # сначала проверяет на список приложенных картинок,
 					  # если нету, то забирает главную картинку
 		gallery_list = product.find('ul', class_='gallery-list js-gallery-list')
