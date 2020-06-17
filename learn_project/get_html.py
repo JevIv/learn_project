8b9550034246a8e317b0823dc212401233dbbe3d
ivan <skeap@cn.ru>
2020-06-17 22:21:58 +0700

    add proxy and fake headers

--- learn_project/get_html.py
+++ learn_project/get_html.py
@@ -1,24 +1,32 @@
 from bs4 import BeautifulSoup as bs
+from fake_headers import Headers
 from time import sleep
+from learn_project.config import PROXY
 import re
 import requests
 
 
 def get_html(url, page_number=None):  # Забирает одну страницу
+
+    header = Headers(headers=True)    # добавляет в request header рандомный user agent
+
     if not page_number:
         params = {}
     else:
         page_number = 1
         params = {'p': page_number}
+
     try:
-        response = requests.get(url, params=params)  # сюда падает текст из ответа на get
-        response.raise_for_status()
+        response = requests.get(url, params=params, proxies=PROXY, headers=header.generate())  # сюда падает текст из ответа на get
+        print(response.raise_for_status())
+        print(response.text)
+        sleep(5)
         return response.text
     except(requests.RequestException, ValueError):
         return False
 
 
-def last_page(text):  # Находит номер последней страницу в категории 
+def last_page(text):  # Находит номер последней страницу в категории
     soup = bs(text, 'html.parser')
     pagination = soup.find_all('span', class_="pagination-item-1WyVp")  # Находит 'кнопки' в разделе pagination
     digits = re.findall(r'\d{2,4}', pagination[-2]['data-marker'],)  # Вытаскивает цифры из предпоследней кнопки
@@ -31,5 +39,4 @@ def get_all_pages(url):  # Делает итерацию по страницам
     max_page = last_page(get_html(url))  # Определает, сколько циклу надо итераций
     for page_number in range(1, max_page):
         all_pages.append(get_html(url, page_number))  # Добавляет страницу (str) как элемент списка
-        sleep(5)
     return all_pages
