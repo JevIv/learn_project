cc1ec7d35625507b056e0e4243da85a5a2848bd7
ivan <skeap@cn.ru>
2020-06-17 22:20:45 +0700

    add proxy and fake headers

--- learn_project/__init__.py
+++ learn_project/__init__.py
@@ -5,6 +5,7 @@ from learn_project import config as cfg
 def create_app():
     app = Flask(__name__)  # создает экземпляр Flask в переменной app
     app.config.from_pyfile('config.py')  # задает файл конфигурационный файл
+
     @app.route('/')  # путь, перейдя по котрому запустится app
     def index():  # возвращает стартовую страничку
         return render_template('index.html')
--- learn_project/config.py
+++ learn_project/config.py
@@ -2,4 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))  # абсолютный путь к файлу конфига
 
+PROXY = {'http': 'http://127.0.0.1:5566',
+         'https': 'https://127.0.0.1:5566'}
+
 TARGET_URL = 'https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/zvukovye_karty-ASgBAgICAkTGB~pm7gm6Zw?cd=1'
