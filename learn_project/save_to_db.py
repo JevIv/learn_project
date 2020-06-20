c6eec89d4438f9f33831bc06a7b5c10eb8adf786
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-06-20 10:31:02 +0100

    testovaja versija sa_to_save_to_db

--- learn_project/save_to_db.py
+++ learn_project/save_to_db.py
@@ -3,8 +3,8 @@ from learn_project.model import db, Products
 
 
 def save_products(d_dict):   #передаём данные в базу
-    products_exists = Products.query.filter(Products.ad_number == d_dict['ad_number']) 		#проверка на дубликаты по номеру обьявления
-    if not products_exists:
-        all_products = Products(name=d_dict['name'], text=d_dict['text'], price=d_dict['price'], address=d_dict['address'], published=d_dict['date'], ad_number=d_dict['ad_number'], images_urls=d_dict['images_urls'])
-        db.session.add(all_products)           										#кладем в сессию базы
-        db.session.commit()                											#сохранение новости в базу
+    #products_exists = Products.query.filter(Products.ad_number == d_dict['ad_number']) 		#проверка на дубликаты по номеру обьявления
+    #if not products_exists:
+    all_products = Products(name=d_dict['name'], text=d_dict['text'], price=d_dict['price'], address=d_dict['address'], published=d_dict['date'], ad_number=d_dict['ad_number'], images_urls=d_dict['images_urls'])
+    db.session.add(all_products)           										#кладем в сессию базы
+    db.session.commit()                											#сохранение новости в базу
