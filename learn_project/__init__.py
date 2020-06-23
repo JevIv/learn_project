3d94dc49c298f5682a06f519d3136b78895da660
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-06-23 20:50:50 +0100

    nakidal products.html

--- learn_project/__init__.py
+++ learn_project/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from learn_project.model import db
+from learn_project.model import db, Products
 
 
 def create_app():
@@ -10,4 +10,12 @@ def create_app():
     @app.route('/')  					 # путь, перейдя по котрому запустится app
     def index():  						 # возвращает стартовую страничку
         return render_template('index.html')
+
+    @app.route('/products')
+    def products():
+        title = 'Товары'
+        products_list = Products.query.order_by(Products.date.desc()).limit(20).all()	        #запрашиваем 20 товаров сортируем по дате
+        return render_template('products.html', page_title=title, products_list=products_list)	#limit(20)-до 20 товаров
+
+
     return app  # возвращает экземпляр приложения Flask
