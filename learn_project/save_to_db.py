31d22cc250938e66ec4741701e4809de13655c97
ivan <skeap@cn.ru>
2020-06-24 23:42:52 +0700

    save straight to db

--- learn_project/save_to_db.py
+++ learn_project/save_to_db.py
@@ -2,19 +2,15 @@ from learn_project.model import db, Products
 from learn_project import create_app
 
 
-def save_products(details_list):  #передаём данные в базу
-    app = create_app()
-    with app.app_context():
-        for details in details_list:
-            products_exists = Products.query.filter(Products.ad_number == details['ad_number']).count()  # проверка на дубликаты по номеру обьявления
-            print(products_exists)
-            if not products_exists:
-                all_products = Products(name=details['name'],
-                                        price=details['price'],
-                                        date=details['date'],
-                                        text=details['text'],
-                                        address=details['address'],
-                                        ad_number=details['ad_number'],
-                                        images_urls=details['images_urls'])
-                db.session.add(all_products)  # кладем в сессию базы
-                db.session.commit()  # сохранение новости в базу
+def save_products(name, price, date, text, address, ad_number, images_urls):  #передаём данные в базу
+    products_exists = Products.query.filter(Products.ad_number == ad_number).count()  # проверка на дубликаты по номеру обьявления
+    if not products_exists:
+        all_products = Products(name=name,
+                                price=price,
+                                date=date,
+                                text=text,
+                                address=address,
+                                ad_number=ad_number,
+                                images_urls=images_urls)
+        db.session.add(all_products)  # кладем в сессию базы
+        db.session.commit()  # сохранение новости в базу
