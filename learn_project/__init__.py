cd2a8eca0e2ebc075d0c0edecb8e335fb966622f
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-06-25 20:30:40 +0100

    added pagination

--- learn_project/__init__.py
+++ learn_project/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from learn_project.model import db, Products
+
+#запуск сервера
+#set FLASK_APP=webapp && set FLASK_ENV=development && 
+#set FLASK_DEBUG=1 && flask run
 
 
 def create_app():
@@ -14,8 +18,21 @@ def create_app():
     @app.route('/products')
     def products():
         title = 'Товары'
-        products_list = Products.query.order_by(Products.date.desc()).limit(20).all()	        #запрашиваем 20 товаров сортируем по дате
-        return render_template('products.html', page_title=title, products_list=products_list)	#limit(20)-до 20 товаров
+        page = request.args.get('page', 1, type=int)
+        products_list = Products.query.order_by(Products.date.desc()).paginate(page,
+                                                                               app.config['ITEMS_PER_PAGE'],
+                                                                               False)
+        prev_url = url_for('products', page=products_list.prev_num) \
+        if products_list.has_prev else None
+
+        next_url = url_for('products', page=products_list.next_num) \
+        if products_list.has_next else None
+
+        return render_template('products.html',
+                                page_title=title,
+                                products_list=products_list.items,
+                                next_url=next_url, 
+                                prev_url=prev_url)
 
 
     return app  # возвращает экземпляр приложения Flask
