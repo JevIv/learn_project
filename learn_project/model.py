c796991bc298893730204c6546d6cfee82a89f98
skeapskeap <65304485+skeapskeap@users.noreply.github.com>
2020-06-29 00:15:32 +0700

    Fix date (#10)
    * add date parser
    
    * fix date parse a little more

--- learn_project/model.py
+++ learn_project/model.py
@@ -8,7 +8,7 @@ class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     price = db.Column(db.String, nullable=False)
-    date = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=False)
     address = db.Column(db.String, nullable=False)
     ad_number = db.Column(db.String, unique=True, nullable=False)
