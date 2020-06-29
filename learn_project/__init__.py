8cd69edaa6e9f207d3246627d04ff3d13eb13c15
ivan <skeap@cn.ru>
2020-06-29 22:59:32 +0700

    users blueprint

--- learn_project/__init__.py
+++ learn_project/__init__.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, current_user, login_required, login_user, logout_user
-from learn_project.model import db, Users
-from learn_project.forms import LoginForm
+from flask import Flask, render_template
+from flask_login import LoginManager, current_user, login_required
+from learn_project.model import db
+from learn_project.user.model import Users
+from learn_project.user.views import blueprint as user_blueprint
 
 
 def create_app():
     app = Flask(__name__)  				 # создает экземпляр Flask в переменной app
     app.config.from_pyfile('config.py')  # задает файл конфигурационный файл
     db.init_app(app)					 # привязывает базу к приложению
-
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'user.login'
+    app.register_blueprint(user_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -21,29 +22,6 @@ def create_app():
     def index():  						 # возвращает стартовую страничку
         return render_template('index.html')
 
-    @app.route('/login')
-    def login():
-        if current_user.is_authenticated:
-            return redirect(url_for('index'))
-        title = 'Авторизация'
-        login_form = LoginForm()
-        return render_template('login.html', page_title=title, form=login_form)
-
-    @app.route('/login-process', methods=['POST'])
-    def login_process():
-        form = LoginForm()
-
-        if form.validate_on_submit:
-            user = Users.query.filter(Users.username == form.username.data).first()
-
-            if user and user.check_password(form.password.data):
-                login_user(user)
-                flash('logged in')
-                return redirect(url_for('index'))
-
-            flash('wrong login or password')
-            return redirect(url_for('login'))
-
     @app.route('/admin')
     @login_required
     def admin_index():
@@ -51,12 +29,5 @@ def create_app():
             return 'Поздравляю! Вы админ.'
         else:
             return 'Вы не админ.'
-
-
-    @app.route('/logout')
-    def logout():
-        logout_user()
-        flash('bb gl hf')
-        return redirect(url_for('index'))
 
     return app  # возвращает экземпляр приложения Flask
--- learn_project/model.py
+++ learn_project/model.py
@@ -1,7 +1,5 @@
 """Опишем наш товар"""
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -18,23 +16,3 @@ class Products(db.Model):
 
     def __repr__(self):
         return '<Products {} {}>'.format(self.id, self.name)
-
-
-class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), index=True, unique=True)
-    password = db.Column(db.String(128))
-    role = db.Column(db.String(10), index=True)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    @property
-    def is_admin(self):
-        return self.role == 'admin'
-
-    def __repr__(self):
-        return '<Users {}>'.format(self.username)
