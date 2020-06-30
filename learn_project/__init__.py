b38cd7c657a6513629a3e94a08c8eb3127916a70
Jev <jevgenijs240693@gmail.com>
2020-06-30 18:52:49 +0100

    Product.html (#12)
    * add proxy and fake headers
    
    * add proxy and fake headers
    
    * sozdal bazu dlja produktov,ostalosj,dopisatj pereda4u dannih iz slovarja v bazu i protestitj
    
    * prokomentiroval
    
    * dopisal bazu i pereda4u infi v bazu, no poka ne rabotaet, nuzno ispravljatj
    
    * ubral ope4atku
    
    * db test
    
    * ispravil save_to_db i podpravil parser.py
    
    * ispravil save_to_db i podpravil parser.py
    
    * testovaja versija sa_to_save_to_db
    
    * добавление данных в бд
    
    * nakidal products.html
    
    * add single ad page
    
    * change ad_page route
    
    * minor fixes
    
    * change ad_page template
    
    * add one more db for image urls
    rename images_urls variable in prod_details
    
    * added pagination
    
    * bug fix
    
    Co-authored-by: ivan <skeap@cn.ru>
    Co-authored-by: skeapskeap <65304485+skeapskeap@users.noreply.github.com>
    Co-authored-by: Gleb Sinyakov <sinyakov@phystech.edu>

--- learn_project/__init__.py
+++ learn_project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from learn_project.model import db, Products, Images
 
 
@@ -11,10 +11,32 @@ def create_app():
     def index():  						 # возвращает стартовую страничку
         return render_template('index.html')
 
+    @app.route('/products')
+    def products():
+        
+        title = 'Товары'
+        page = request.args.get('page', 1, type=int)
+        products_list = Products.query.order_by(Products.date.desc()).paginate(page,
+                                                                               app.config['ITEMS_PER_PAGE'],
+                                                                               False)
+        prev_url = url_for('products', page=products_list.prev_num) \
+        if products_list.has_prev else None
+
+        next_url = url_for('products', page=products_list.next_num) \
+        if products_list.has_next else None
+
+        return render_template('products.html',
+                                page_title=title,
+                                products_list=products_list.items,
+                                next_url=next_url, 
+                                prev_url=prev_url)
+
+
     @app.route('/ad_page/<prod_db_id>')
     def ad_page(prod_db_id):
         ad_items = Products.query.get(prod_db_id)
         image_urls = Images.query.filter_by(product_id=prod_db_id).all()  # возвращает список объектов класса
         image_urls = [image_url.img_url for image_url in image_urls]      # вытаскиваем из этих объектов ссылки и кладём в список
         return render_template('ad_page.html', ad_items=ad_items, image_urls=image_urls)
+
     return app  # возвращает экземпляр приложения Flask
--- learn_project/config.py
+++ learn_project/config.py
@@ -8,3 +8,4 @@ PROXY = {'http': 'http://127.0.0.1:5566',
 TARGET_URL = 'https://www.avito.ru/novosibirsk/drugie_zhivotnye/horki-ASgBAgICAUSyA65L?cd=1'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'learn_project.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+ITEMS_PER_PAGE = 5
