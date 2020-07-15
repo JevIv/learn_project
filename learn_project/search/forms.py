ee3ae8e6967c1366a4dcd2b1ee1f4748786f2b05
Evgeny Ivanov <jevgenijs240693@gmail.com>
2020-07-15 06:26:58 +0100

    alternative solution

--- learn_project/search/forms.py
+++ learn_project/search/forms.py
@@ -1,6 +1,7 @@
+"""from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class SearchForm(Form):
+class SearchForm(FlaskForm):
   search = StringField('Поиск', [DataRequired()], render_kw={"class": "form-control mr-sm-2"})
-  submit = SubmitField('Поиск', render_kw={'class': 'btn btn-outline-success my-2 my-sm-0'})
+  submit = SubmitField('Поиск', render_kw={'class': 'btn btn-outline-success my-2 my-sm-0'})"""
--- learn_project/search/views.py
+++ learn_project/search/views.py
@@ -1,25 +1,32 @@
-from flask import Blueprint, flash, redirect, render_template, url_for
+"""from flask import Blueprint, flash, redirect, render_template, url_for
 from learn_project.search.forms import SearchForm
 
 
 blueprint = Blueprint('search', __name__)
 
 
-
 @blueprint.route('/search', methods=['GET', 'POST'])
 def search():
+	query = request.args.get('query')  
+	        query = request.args.get('query')  
+	        if query:
+	            results = Products.query.filter(Products.name.contains(query) | Products.text.contains(query)).all()
+	        else:
+	            results = flash('По запросу ничего не найдено!')						 # возвращает стартовую страничку
+	            return redirect('/')
+
+
 	results = []
-    form = SearchForm()
-    search_item = form.search.data
-    if request.method == 'POST' and form.validate_on_submit():
-    	
-    	if search_item == '':
-        	qry = db_session.query(Products)
-        	results = qry.all()
-	    if not results:
-	    	flash('По запросу ничего не найдено!')
-	    	return redirect('/')
+	form = SearchForm()
+	search_item = form.search.data
+	if request.method == 'POST' and form.validate_on_submit():
+		if search_item == '':
+			qry = db_session.query(Products)
+			results = qry.all()
+		if not results:
+			flash('По запросу ничего не найдено!')
+			return redirect('/')
 		else:
-	    	return render_template('base.html', 
+			return render_template('navbar.html', 
 	    							form=form,
-	    							results=results)
+	    							results=results)"""
