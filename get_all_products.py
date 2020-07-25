b9658cd86374d9e9cd8a24db14384ffada248a62
Jev <jevgenijs240693@gmail.com>
2020-07-25 15:06:00 +0100

    search form (#20)
    * add base template
    
    * add navbar
    
    * blueprints for adverts
    
    * добавил страничку new_ad
    
    * добавил форму нового объявления
    
    * save new_ad to db
    
    * generate ad_number. proper date presentation
    
    * add new advert
    
    * dobavil kommentarii v model,forms,init.py
    
    * add current  user's ads page
    
    * sobral modulj komentarii
    
    * sold and hide buttons
    
    * complete edit ad functions
    
    * minor fixes
    
    * fix urls at main page
    
    * make images clickable
    
    * search form
    
    * Fixed comments model,form,views
    
    * fixed comments model, added utils.py
    
    * bug fix
    
    * фикс для неавторизованных пользователей
    
    * Update views.py
    
    удалил лишний роут
    
    * comment bug fixs
    
    * bug fix
    
    * bug fix
    
    * base commit
    
    * alternative solution
    
    * bug fix
    
    * bug fix
    
    Co-authored-by: ivan <skeap@cn.ru>
    Co-authored-by: skeapskeap <65304485+skeapskeap@users.noreply.github.com>

--- get_all_products.py
+++
@@ -1,7 +0,0 @@
-""""""
-from learn_project import create_app
-from learn_project.parser import parse
-
-app = create_app()
-with app.app_context():
-	parse()
---
+++ learn_project/search/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, flash, redirect, render_template, request
+from learn_project.advert.model import Products
+
+
+blueprint = Blueprint('search', __name__,url_prefix='/')
+
+
+@blueprint.route('/search')
+def search():  
+    query = request.args.get('query') 
+    if query:
+        results = Products.query.filter(Products.name.contains(query) | Products.text.contains(query)).all()
+        for result in results:
+        	results_id    = result.query.get(result.id)
+        if not results:
+        	results = flash('По запросу ничего не найдено!')
+    else:
+    	results = flash('По запросу ничего не найдено!')
+    return render_template('search.html', page_title='Тут вам не авито!', results=results, results_id=results_id)
